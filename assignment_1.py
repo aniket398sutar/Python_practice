7dac1505e828a5c390b88f29666d45a71f417750
aniket suutar <aniket398sutar@gmail.com>
2025-10-08 19:57:07 +0530

    Assignment task completed

---
+++ assignment_1.py
@@ -0,0 +1,10 @@
+#addition code
+
+a = input("Enter the first number")
+
+b = input("Enter the second number")
+
+print("addition : ", int(a)+int(b))
+print("subtraction : ", int(a)-int(b))
+print("division : ", int(a)/int(b))
+print("multiplication: ", int(a)*int(b))
---
+++ assignment_1_1.py
@@ -0,0 +1,9 @@
+'''
+
+cocatenating 2 user input string with default welcome message
+'''
+
+first = input("enter your first name : ")
+second = input("enter your second name : ")
+
+print("hello, " + first + " " + second + "! Welcome to the python program.")
