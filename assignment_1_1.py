d22f75ac25db1a3fa21bbca20f52752b4e3d02ed
aniket398sutar <65346247+aniket398sutar@users.noreply.github.com>
2025-10-15 23:16:12 +0530

    Delete assignment_1_1.py

--- assignment_1_1.py
+++
@@ -1,9 +0,0 @@
-'''
-
-cocatenating 2 user input string with default welcome message
-'''
-
-first = input("enter your first name : ")
-second = input("enter your second name : ")
-
-print("hello, " + first + " " + second + "! Welcome to the python program.")
